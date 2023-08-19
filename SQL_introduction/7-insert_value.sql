7e64182255bdceeb0356c385f4806e9d381d3c1a
Wairimu <charitykabuu21@gmail.com>
2023-08-20 00:12:51 +0300

    Update 7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,6 +1,3 @@
--- insert a new row in a table
-
-INSERT INTO hbtn_test_db_7.first_table (
-  ID (89),
-  NAME (Holberton School),
-  );
+-- insert into a table
+INSERT INTO hbtn_test_db_7.first_table (ID, NAME)
+VALUES (89, 'Holberton School');
