0544b99a71f3e847d169ec2a8dec82935f29e8ca
Wairimu <charitykabuu21@gmail.com>
2023-08-19 09:21:52 +0300

    Update 0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,7 +1,7 @@
 -- Connect to the MySQL server and list initial databases sorted
 USE mysql;
 
--- Show the initial list of databases sorted
+-- Initial databases sorted
 SELECT SCHEMA_NAME
 FROM INFORMATION_SCHEMA.SCHEMATA
 ORDER BY SCHEMA_NAME;
@@ -9,7 +9,7 @@ ORDER BY SCHEMA_NAME;
 -- Create a new database
 CREATE DATABASE new_database;
 
--- Show the list of databases after creating a new one
+-- Databases after creation of multiple databases
 SELECT SCHEMA_NAME
 FROM INFORMATION_SCHEMA.SCHEMATA
 ORDER BY SCHEMA_NAME;
