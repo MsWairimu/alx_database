475fb16498db84a52f201a0133586f77215be0d6
Wairimu <charitykabuu21@gmail.com>
2023-08-19 22:28:19 +0300

    Update 0-list_databases.sql

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,16 +1,3 @@
--- Retrieve the first 2 students from Batch ID=2
--- because Batch 2 is the best!
-SELECT id, name
-FROM students
-WHERE batch_id = 2
-ORDER BY created_at DESC
-LIMIT 2;
+-- this script lists all database in my workspace
 
--- Create a new user 'Mswairimu'@'localhost'
-CREATE USER 'Mswairimu'@'localhost';
-
--- Retrieve a list of databases
-SHOW DATABASES;
-
-SELECT name FROM sys.databases
-GO
+SHOW DATABASES
