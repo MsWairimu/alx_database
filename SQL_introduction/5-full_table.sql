77700d2102ad6199dafbbdfb89e2bcf1927b94d5
Wairimu <charitykabuu21@gmail.com>
2023-08-19 23:26:03 +0300

    Update 5-full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- write script that prints the full description of the table first_table from hbtn_0c_0 database
 
-SHOW CREATE TABLE FROM hbtn_test_db_5.first_table;
+SHOW CREATE TABLE FROM hbtn_test_db_5.first_table
