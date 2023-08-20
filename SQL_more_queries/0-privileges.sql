e7a07e24bd64b66dcd89efd7c6bec6445fb11024
Wairimu <charitykabuu21@gmail.com>
2023-08-21 00:09:01 +0300

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,22 +1,4 @@
 -- this script lists all privileges of a MYSQL server
-SELECT
-    user,
-    host,
-    Grant_priv,
-    Create_priv,
-    Insert_priv,
-    Update_priv,
-    Delete_priv,
-    Select_priv,
-    Show_view_priv,
-    Create_view_priv,
-    Alter_priv,
-    Create_routine_priv,
-    Execute_priv,
-    Trigger_priv
-FROM
-    mysql.user
-WHERE
-    user IN ('user_0d_1', 'user_0d_2');
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
