1075c53083f199867bc46ab5acdf2fde0e7a05bb
Wairimu <charitykabuu21@gmail.com>
2023-08-21 01:02:21 +0300

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,1 +1,12 @@
+-- this script that creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
+-- Create the table cities if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL
+);
 
