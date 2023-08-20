28c6ccca97d275484f8609f0759b18453d2a309e
Wairimu <charitykabuu21@gmail.com>
2023-08-21 00:29:03 +0300

    Update 3-force_name.sql

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- this script creates the table force_name on the MYSQL server
+-- This script creates the table force_name on the MYSQL server
 CREATE TABLE IF NOT EXISTS force_name (
   ID INT,
   NAME VARCHAR(256) NOT NULL
-  );
+);
