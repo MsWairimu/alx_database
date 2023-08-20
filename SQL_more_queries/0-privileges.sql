f129e43dce0f5fe8c22f720f8554fd7e069321bc
Wairimu <charitykabuu21@gmail.com>
2023-08-21 00:02:03 +0300

    Create 0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,19 @@
+SELECT
+    user,
+    host,
+    Grant_priv,
+    Create_priv,
+    Insert_priv,
+    Update_priv,
+    Delete_priv,
+    Select_priv,
+    Show_view_priv,
+    Create_view_priv,
+    Alter_priv,
+    Create_routine_priv,
+    Execute_priv,
+    Trigger_priv
+FROM
+    mysql.user
+WHERE
+    user IN ('user_0d_1', 'user_0d_2');
